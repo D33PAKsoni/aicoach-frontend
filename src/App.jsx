d87af582c60c4f851cba7337b0f95f45ed0c797c
Deepak Soni <deep.x.lis@gmail.com>
2026-02-27 16:05:39 +0530

    no mobile

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ import InterviewInstructions from './pages/InterviewInstructions';
 
 export const App = () => {
   return (
-    <div>
+    <div className='wholepage'>
       {/* <Landing /> */}
       <AuthProvider>
         <BrowserRouter>
